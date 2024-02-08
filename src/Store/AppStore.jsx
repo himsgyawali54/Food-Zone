7c9327e2ed61569189d272274c5a86d105efa5b0
Himani gyawali <hebika1234@gmail.com>
2024-02-08 13:39:08 +0545

    reducers are combined

--- src/Store/AppStore.jsx
+++ src/Store/AppStore.jsx
@@ -4,21 +4,15 @@ import foodItemsReducer from "../feature/AppReducer";
 import { userAuthApi } from "../Api/UserApi";
 import loginuserReducer from "../feature/UserSlice";
 
-// const rootReducer = combineReducers({
-//   foodItems: foodItemsReducer,
-//   loginUser: loginuserReducer,
-// });
+const rootReducer = combineReducers({
+  foodItems: foodItemsReducer,
+  loginUser: loginuserReducer,
+  [fooditemsApi.reducerPath]: fooditemsApi.reducer,
+  [userAuthApi.reducerPath]: userAuthApi.reducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    // rootReducer,
-    // [fooditemsApi.reducerPath]: fooditemsApi.reducer,
-    // [userAuthApi.reducerPath]: userAuthApi.reducer,
-    foodItems: foodItemsReducer,
-    [fooditemsApi.reducerPath]: fooditemsApi.reducer,
-    loginUser: loginuserReducer,
-    [userAuthApi.reducerPath]: userAuthApi.reducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       fooditemsApi.middleware,
