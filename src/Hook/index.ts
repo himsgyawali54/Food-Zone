eaf103d4b87a71f6c099640f3a5b0756e9fcc63e
Himani gyawali <hebika1234@gmail.com>
2024-03-22 07:55:06 +0545

    added useAppDispatch and useAppSelector

--- src/Hook/index.ts
+++ src/Hook/index.ts
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
 
 // Store
-// import type { RootState, AppDispatch } from "redux-toolkit/store";
+import type { RootState, AppDispatch } from "../Store/AppStore";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-// export const useAppDispatch: () => AppDispatch = useDispatch;
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
