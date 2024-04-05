e0bc755700690f5fe1be8a46151ad4f63e95f545
Himani gyawali <hebika1234@gmail.com>
2024-04-05 12:45:44 +0545

    tsconfig and api are  updated

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -1,14 +1,54 @@
 import React from "react";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const fooditemsApi = createApi({
-  reducerPath: "fooditemsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3031/" }),
-  endpoints: (builder) => ({
-    getFoodItems: builder.query({
-      query: () => "foodItems",
-    }),
-  }),
+// export const fooditemsApi = createApi({
+//   reducerPath: "fooditemsApi",
+//   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3031/" }),
+//   endpoints: (builder) => ({
+//     getFoodItems: builder.query({
+//       query: () => "foodItems",
+//     }),
+//   }),
+// });
+
+// export const { useGetFoodItemsQuery } = fooditemsApi;
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// Assets
+import { BASE_URL } from "../constants/endpoint";
+// Store
+// import { logOut } from "../features/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  prepareHeaders: (headers) => {
+    const writerUserInfo = localStorage.getItem("writerUserInfo");
+    const token = writerUserInfo
+      ? JSON.parse(writerUserInfo ?? "")?.token
+      : null;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
 });
 
-export const { useGetFoodItemsQuery } = fooditemsApi;
+const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 403) {
+    // Log out the user
+    // api.dispatch(logOut());
+    // Optionally, clear local storage
+    localStorage.clear();
+  }
+
+  return result;
+};
+
+export const apiSlice = createApi({
+  tagTypes: ["userInfo"],
+  baseQuery: baseQueryWithReauth,
+  endpoints: () => ({}),
+});
--- src/Store/AppStore.ts
+++ src/Store/AppStore.ts
@@ -1,5 +1,5 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { fooditemsApi } from "../Api/Api";
+// import { fooditemsApi } from "../Api/Api";
 import foodItemsReducer from "../feature/AppReducer";
 import { userAuthApi } from "../Api/UserApi";
 import loginuserReducer from "../feature/UserSlice";
@@ -9,7 +9,7 @@ const rootReducer = combineReducers({
   foodItems: foodItemsReducer,
   loginUser: loginuserReducer,
   register: registerReducer,
-  [fooditemsApi.reducerPath]: fooditemsApi.reducer,
+  // [fooditemsApi.reducerPath]: fooditemsApi.reducer,
   [userAuthApi.reducerPath]: userAuthApi.reducer,
 });
 
@@ -17,7 +17,7 @@ export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
-      fooditemsApi.middleware,
+      // fooditemsApi.middleware,
       userAuthApi.middleware
     ),
 });
